9639440ea049671d2f3c454591d97466d57908c0
Brian Mearns <mearns.b@gmail.com>
2018-07-18 21:00:27 -0400

    Revert the Config-as-Resource change
    Going back to just using a simple config API. Hard to see why this wouldn't be
    a long term strategy.

--- src/stacks/npm.js
+++ src/stacks/npm.js
@@ -5,13 +5,10 @@ export function provideResources (provide) {
 
 export function converge ({config, resource}) {
   const jsonFile = resource('JsonFile')
-  resource('config')({name: 'project.name', version: 'project.version'}, ({name, version}) => {
-    jsonFile({
-      path: 'package.json',
-      contentBody: {
-        name,
-        version
-      }
-    })
+  jsonFile({
+    path: 'package.json',
+    contentBody: {
+      name: config('project.name')
+    }
   })
 }
