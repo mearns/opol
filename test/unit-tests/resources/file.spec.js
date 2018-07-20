1c03f6c5f13f791d002a5f06aff774d18adbf07f
Brian Mearns <mearns.b@gmail.com>
2018-07-20 15:32:09 -0400

    Add unit test for file resource

--- test/unit-tests/resources/file.spec.js
+++ test/unit-tests/resources/file.spec.js
@@ -5,6 +5,7 @@
 import {File} from '../../../src/resources/file'
 
 // Support
+import {Resource} from '../../../src/resource'
 import {opolTest} from '../../../test-util'
 import path from 'path'
 import chai, {expect} from 'chai'
@@ -29,9 +30,64 @@ describe('the file resource', () => {
       expect(writeFileStub).to.have.been.calledOnceWithExactly(EXPECTED_PATH, TEST_CONTENT)
     })
   })
+
+  it('should write the last content provided for each file', () => {
+    // given
+    const TEST_PATH = 'rel/path/to/file.ext'
+    const TEST_CONTENT_1 = 'here is an early version'
+    const TEST_CONTENT_2 = 'here is another version'
+    const TEST_CONTENT_FINAL = 'this is the expected test content'
+    const EXPECTED_PATH = path.resolve(TEST_PATH)
+
+    // expect
+    return testFileResource((fileResource) => {
+      fileResource({path: TEST_PATH, content: TEST_CONTENT_1})
+      fileResource({path: TEST_PATH, content: TEST_CONTENT_2})
+      fileResource({path: TEST_PATH, content: TEST_CONTENT_FINAL})
+    }).then(({mkdirpStub, writeFileStub}) => {
+      expect(writeFileStub).to.have.been.calledOnce
+      expect(writeFileStub).to.have.been.calledWithExactly(EXPECTED_PATH, TEST_CONTENT_FINAL)
+    })
+  })
+
+  it('should use a provided function to get the file-contents during the execute stage', () => {
+    // given
+    const TEST_PATH = 'rel/path/to/file.ext'
+    const EXPECTED_PATH = path.resolve(TEST_PATH)
+    const EXPECTED_CONTENT = 'this is the expected content'
+    class TestResource extends Resource {
+      constructor (api) {
+        super(api)
+        this._content = 'this is the wrong content'
+      }
+
+      prepAndValidateInstance () {
+        this.resource('File')({path: TEST_PATH, content: () => this._content})
+      }
+
+      executeInstance () {
+        this._content = EXPECTED_CONTENT
+      }
+    }
+
+    // when
+    return testFileResource(
+      () => {},
+      opolTest => {
+        opolTest.withMockResource('--test-resource--', TestResource)
+        opolTest.withExerciser(({resource}) => {
+          resource('--test-resource--')()
+        })
+      }
+    )
+      .then(({writeFileStub}) => {
+        expect(writeFileStub).to.have.been.calledOnce
+        expect(writeFileStub).to.have.been.calledWith(EXPECTED_PATH, EXPECTED_CONTENT)
+      })
+  })
 })
 
-function testFileResource (when) {
+function testFileResource (when, opolTestSetup = () => {}) {
   const writeFileStub = sinon.stub().returns(Promise.resolve())
   const mkdirpStub = sinon.stub().returns(Promise.resolve())
 
@@ -41,12 +97,11 @@ function testFileResource (when) {
     }
   }
 
-  // when
-  return opolTest()
-    .withMockResource('File', MockFileResource)
-    .withExerciser(({resource}) => {
-      when(resource('File'))
-    })
+  const test = opolTest().withMockResource('File', MockFileResource).withExerciser(({resource}) => {
+    when(resource('File'))
+  })
+  opolTestSetup(test)
+  return test
     .testConverge()
     .then(() => ({writeFileStub, mkdirpStub}))
 }
